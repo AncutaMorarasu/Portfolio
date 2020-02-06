dbb7a8662b3e53d8c625b7a88e859c1f5a2e6dd9
AnnieisOK <48488054+AnnieisOK@users.noreply.github.com>
2020-02-06 15:13:46 +0200

    Delete footer.js

--- Furniture Inc/scripts/footer.js
+++
@@ -1,6 +0,0 @@
-    document.addEventListener('DOMContentLoaded', function(){
-        // console.log("hello");
-        var fullYear = document.getElementById('address')
-       
-        // console.log(fullYear);
-    });
